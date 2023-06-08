74b7c879aff4f0eccab403fa934aa861c3451d1e
19Edwin92 <b_a_s_e@naver.com>
2023-06-08 16:52:18 +0900

    상세페이지 - 레이아웃 디자인 적용 #3

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, render_template, request, jsonify
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+  return render_template('index.html')
+
+@app.route('/test')
+def test():
+  return render_template('edwinTest.html')
+
+@app.route('/main')
+def main():
+  return render_template('edwinDetail.html')
+
+@app.route('/detail')
+def detail():
+  return render_template('detail.html')
+
+
+if __name__ == '__main__':
+  app.run('0.0.0.0', port=5000, debug=True)
